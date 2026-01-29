0d6b1bb2592f2db25dcfdaa1735c7cf8fba8f70c
DammySmartSolutions <osademiluyi@mun.ca>
2026-01-29 18:26:12 -0330

    Initial Commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { getAllnotes, getNoteById, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
+
+const router = express.Router();
+
+
+router.get('/', getAllnotes) ;
+router.get('/:id', getNoteById) ;
+router.post('/', createNote); 
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
+
+
+
+
+
+
+
+
+export default router;
+
+
