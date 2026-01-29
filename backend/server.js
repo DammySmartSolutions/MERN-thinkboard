076e2ab63a0932b9fb8843b692e0c6812b54dae1
DammySmartSolutions <osademiluyi@mun.ca>
2026-01-29 19:15:21 -0330

    prepare for the deployment

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,40 @@ import dotenv from "dotenv";
 import notesRoutes from "./src/routes/notesRoutes.js";
 import { connectDB } from "./src/config/db.js";
 import rateLimiter from "./src/middleware/rateLimiter.js";
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve()
 
 
 
 // middleware
 
+if(process.env.NODE_ENV !== "production")
+{
+    app.use(cors({
+        origin: "http://localhost:5173",
 
-app.use(cors({
-    origin: "http://localhost:5173",
-
-}));
+    }));
+};
 app.use(express.json());
 
 app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production")
+{
+
+    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname,"../frontend/dist","index.html"));
+
+    });
+}
 
 connectDB().then(()=>  {
     app.listen(PORT, () => {
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -28,6 +28,7 @@ const NoteCard = ({note, setNotes}) => {
     };
   return (
     <div>
+         
       <Link to ={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 
       border-t-4 border-solid border-[#00FF9D]">
         
